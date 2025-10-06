4ecea5ecad4038cf63caae2e8a16357c68afdd66
Chimezie Onwuegbuchulem <137625845+31z3m1hC@users.noreply.github.com>
2025-10-06 11:35:04 -0400

    Update MainActivity.kt

--- app/src/main/java/com/unh/personal_health_buddy/MainActivity.kt
+++ app/src/main/java/com/unh/personal_health_buddy/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 🔹 Initialize Firebase
+        //  Initialize Firebase
         FirebaseApp.initializeApp(this)
 
         enableEdgeToEdge()
